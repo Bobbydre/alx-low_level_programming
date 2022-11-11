07e5a597bf983b19ab6bd74642edca1ec4b6c617
bobbydre <dareboboye@gmail.com>
2022-11-11 13:30:30 -0800

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdlib.h>
 
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/**
+* malloc_checked - allocates memory.
+* @b: amount of bytes.
+* Return: pointer to the allocated memory.
+* if malloc fails, status value is equal to 98.
+*/
 
-#endif
+void *malloc_checked(unsigned int b)
+
+{
+	char *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+
+}
